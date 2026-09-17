0950d9d3607fda14ad141dfdfd3a30f8a3ec771c
mntjams <matouszatloukal@seznam.cz>
2026-09-17 17:17:32 +0200

    Run only 48 cores benchmark and disable SMT

--- run_local_benchmarks.sh
+++ run_local_benchmarks.sh
@@ -3,13 +3,6 @@
 set -ueo pipefail
 
 NODE="bw01"
+CORES=48
 
-for CORES in 16 32 48; do
-    echo "=========================================================="
-    echo "Starting PoisFFT benchmarks with ${CORES} cores..."
-    echo "=========================================================="
-
-    srun -p gpu-short-teach -N 1 --ntasks-per-node 1 -w "$NODE" -c "$CORES" -t 02:00:00 ch-run -b mapped:/opt/build imgdir -- /bin/bash -c "cd /opt/build/PoisFFT && ./local_benchmarks.sh ./bin/gcc/results/cores_${CORES}"
-done
-
-echo "All multi-core sweeps completed for PoisFFT."
+srun -p gpu-ffa-gpulab -N 1 --ntasks-per-node 1 -w "$NODE" -c "$CORES" --threads-per-core=1 -t 12:00:00 ch-run -b mapped:/opt/build imgdir -- /bin/bash -c "cd /opt/build/PoisFFT && ./local_benchmarks.sh ./bin/gcc/results/cores_${CORES}"
